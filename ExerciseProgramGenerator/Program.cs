e890a87a675772ae0ee39c9523a3f00ff1231024
Maja Daniela Hansen <mdha54168.ucl.dk>
2023-04-23 14:16:20 +0200

    Add project files.

---
+++ ExerciseProgramGenerator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ExerciseProgramGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
