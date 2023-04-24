c2fdedaa5078f8e0873d6dbcfa4895c088bbf2a1
Maja Daniela Hansen <mdha54168.ucl.dk>
2023-04-24 10:16:26 +0200

    Testing out multiple exercises

--- ExerciseProgramGenerator/Program.cs
+++ ExerciseProgramGenerator/Program.cs
@@ -7,9 +7,13 @@ namespace ExerciseProgramGenerator
         static void Main(string[] args)
         {
             Exercise exercise = new Exercise("Downward Dog", 3, 1, "Leg");
+            Exercise exercise2 = new Exercise("Upward Dog", 3, 1, "Leg");
+
+            
 
             DataHandler handler = new DataHandler("Data.txt");
             handler.SaveExercise(exercise);
+            handler.SaveExercise(exercise2);
             handler.LoadExercise();
 
             Console.Write("Writing Exercise: ");
